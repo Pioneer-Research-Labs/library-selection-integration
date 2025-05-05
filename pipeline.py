259d9bb9ad503218ef38b0ccde6a85b763f6ccd4
Jonathan Liu <jupyter-jhkliu42@ip-172-31-5-159.us-west-1.compute.internal>
2025-05-05 23:04:59 +0000

    updating metadata parsing for fitness calculations

--- pipeline.py
+++ pipeline.py
@@ -1,46 +1,65 @@
 ### Master pipeline
+### Calculates fitness for each unique library/strain/condition combination
 ### TODO: integration with long read data
 import argparse
 from os.path import join
 from calculate_fitness_matrix import (
+    load_metadata,
     load_and_merge_data,
     calculate_psi_freq,
     calculate_fitness
 )
 
 if __name__ == '__main__':
-    # Get short read results path and metadata file path
+    # Get short read results path and metadata filename
     parser = argparse.ArgumentParser(
         description="Pipeline for calculating fitness and integrating with library data.")
     parser.add_argument("--short_read_path", type=str, required=True, help="Path to short read results.")
-    parser.add_argument("--metadata", type=str, required=False, help="Name of metadata file.")
-    parser.add_argument("--base_timepoint", type=int, required=False, help="Base timepoint to use as reference.")
+    parser.add_argument("--metadata", type=str, required=False, help="Name of metadata file (default='metadata.csv').")
+    parser.add_argument("--out_prefix", type=str, required=False, help="Name of output fitness file prefix (default='fitness').")
+    parser.add_argument("--base_timepoint", type=int, required=False, help="Base timepoint to use as reference (default=0).")
 
     args = parser.parse_args()
 
     short_read_path = args.short_read_path
     if args.metadata:
-        metadata_path = args.metadata
+        metadata_file = args.metadata
     else:
-        metadata_path = 'metadata.csv'
+        metadata_file = 'metadata.csv'
+    if args.out_prefix:
+        out_prefix = args.out_prefix
+    else:
+        out_prefix = 'fitness'
     if args.base_timepoint:
         base_timepoint = args.base_timepoint
     else:
         base_timepoint = 0
 
-    # Load data
-    print('Loading data...')
-    counts_merge = load_and_merge_data(short_read_path, metadata_file=metadata_path)
-    print('Data loaded.')
+    # Load metadata
+    print('Loading metadata...')
+    sample_dict, metadata = load_metadata(short_read_path, metadata_file)
+    print('Metadata loaded.')
 
-    # Calculate psi-freq
-    print('Calculating psi-freq...')
-    df_psi_freq = calculate_psi_freq(counts_merge, base_timepoint=base_timepoint)
+    # For each library/condition/strain combination, run the pipeline
+    groups = sample_dict.keys()
+    for g in groups:
+        samples = sample_dict[g]
+        print('Processing group: ', g)
 
-    # Calculate fitness
-    print('Calculating fitness...')
-    df_fitness = calculate_fitness(counts_merge, df_psi_freq, base_timepoint=base_timepoint)
-    
-    # Save fitness data
-    print('Saving fitness data...')
-    df_fitness.to_parquet(join(short_read_path, 'fitness.parquet'), index=False)
+        # Load data
+        print('Loading data...')
+        counts_merge = load_and_merge_data(short_read_path, metadata, samples)
+        print('Data loaded.')
+
+        # Calculate psi-freq
+        print('Calculating psi-freq...')
+        df_psi_freq = calculate_psi_freq(counts_merge, base_timepoint=base_timepoint)
+
+        # Calculate fitness
+        print('Calculating fitness...')
+        df_fitness = calculate_fitness(counts_merge, df_psi_freq, base_timepoint=base_timepoint)
+        
+        # Save fitness data
+        out_name = out_prefix + '_' + str(g[0]) + '_' + str(g[1]) + '_' + str(g[2])
+        print('Saving fitness data...')
+        df_fitness.to_parquet(join(short_read_path, out_name + '.parquet'), index=False)
