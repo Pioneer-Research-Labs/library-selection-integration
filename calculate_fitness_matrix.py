6e3f0c0dcd29ff62774689b66c61617762f01e57
Jonathan Liu <jupyter-jhkliu42@ip-172-31-5-159.us-west-1.compute.internal>
2025-05-14 22:24:07 +0000

    fixing some barcode computations

--- calculate_fitness_matrix.py
+++ calculate_fitness_matrix.py
@@ -157,13 +157,13 @@ def calculate_fitness(counts_merge, df_psi_freq, keep_dropouts=True, base_timepo
     df_fitness = pd.merge(
         df_fitness, counts_merge[merge_cols], on=['library', 'condition', 'strain', 'replicate', 'timepoint', 'barcode'], how='left')
 
-    # Keep only barcodes that exist in at least two nonzero timepoints
-    print('Filtering for barcodes that exist in at least two nonzero timepoints')
+    # Keep only barcodes that exist in t=1 timepoint
+    print('Filtering for barcodes that exist in first nonzero timepoint.')
     print('Number of barcodes before filtering: ', len(df_fitness.barcode.unique()))
 
-    timepoint_counts = df_fitness.groupby(
+    timepoint_counts = df_fitness[df_fitness.timepoint == base_timepoint + 1].groupby(
         ['library','condition','strain','replicate','barcode'])['n'].count()
-    bcs_to_keep = timepoint_counts[timepoint_counts >= 2].index
+    bcs_to_keep = timepoint_counts[timepoint_counts > 0].index
     df_fitness.set_index(['library','condition','strain','replicate','barcode'], inplace=True)
     df_fitness = df_fitness.loc[bcs_to_keep]
     df_fitness.reset_index(inplace=True)
